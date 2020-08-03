79bbfd2c323f2d6c0ad2fc58f4625501da91fa21
David Konerding <dakoner@gmail.com>
2020-08-03 16:20:58 -0700

    Replace USB-based RAMPS with grblesp32 network

--- heliostat_ui/heliostat_ui.py
+++ heliostat_ui/heliostat_ui.py
@@ -1,17 +1,26 @@
 from PyQt5 import QtWidgets, QtCore, uic
 import sys 
 import os
-from ramps_qobject import QRAMPSObject
+from grblesp32_qobject import GRBLESP32Client
 from gps_qobject import GPSQObject
 from sun_pos import getSunPos
 import pynmea2
+
+STATE_INIT=0
+STATE_HOMED_X=1
+STATE_HOMED_Y=2
+STATE_SET_WORK=3
+STATE_READY=4
+STATE_SENDING_COMMAND=5
+STATE_ERROR=-1
+
 class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
         #Load the UI Page
-        uic.loadUi('heliostat_ui\heliostat.ui', self)
+        uic.loadUi('heliostat.ui', self)
         self.down_button.clicked.connect(self.down_button_clicked)
         self.up_button.clicked.connect(self.up_button_clicked)
         self.right_button.clicked.connect(self.right_button_clicked)
@@ -19,15 +28,17 @@ class MainWindow(QtWidgets.QMainWindow):
         self.home_x_button.clicked.connect(self.home_x_button_clicked)
         self.home_y_button.clicked.connect(self.home_y_button_clicked)
 
-        #self.ramps_qobject = QRAMPSObject()
-        #self.ramps_qobject.messageSignal.connect(self.on_ramps_read)
+        self.grblesp32 = GRBLESP32Client()
+        self.grblesp32.messageSignal.connect(self.on_ramps_read)
 
         self.gps_qobject = GPSQObject()
-        self.gps_qobject.messageSignal.connect(self.on_gps_read)
 
-        # TODO(dek): instead of a timer, use state machine that waits for the expected startup string
         self.ramps_input.returnPressed.connect(self.line_entered)
 
+        self.gps_qobject.messageSignal.connect(self.on_gps_read)
+
+        self.state = STATE_INIT
+        self.send_line("$HX")
 
     def line_entered(self):
         line = self.ramps_input.text()
@@ -37,9 +48,44 @@ class MainWindow(QtWidgets.QMainWindow):
     def send_line(self, line):
         self.ramps_output.insertPlainText("Sent: " + line)
         self.ramps_output.insertPlainText("\n")
-        #self.ramps_qobject.send_line(line)
+        self.grblesp32.send_line(line)
 
     def on_ramps_read(self, data):
+        d = data.strip()
+        if self.state == STATE_INIT:
+            print("in STATE_INIT, got", d)
+            if d == 'ok':
+                print("got ok")
+                self.state = STATE_HOMED_X
+                self.send_line("$HY")
+            else:
+                print("got unexpected data:", d)
+                self.state = STATE_ERROR
+        elif self.state == STATE_HOMED_X:
+            print("in STATE_HOMED_X, got", d)
+            if d == 'ok':
+                print("got ok")
+                self.state = STATE_HOMED_Y
+                self.send_line("G10 L20 P1 X0 Y0")
+            else:
+                print("got unexpected data:", d)
+                self.state = STATE_ERROR
+        elif self.state == STATE_HOMED_Y:
+            print("in STATE_HOMED_X, got", d)
+            if d == 'ok':
+                print("got ok")
+                self.state = STATE_READY
+            else:
+                print("got unexpected data:", d)
+                self.state = STATE_ERROR    
+        elif self.state == STATE_SENDING_COMMAND:
+            print("in STATE_SENDING_COMMAND, got", d)
+            if d == 'ok':
+                print("got ok")
+                self.state = STATE_READY
+            else:
+                print("got unexpected data:", d)
+                self.state = STATE_ERROR  
         self.ramps_output.insertPlainText(data)
         self.ramps_output.verticalScrollBar().setValue(self.ramps_output.verticalScrollBar().maximum())
 
@@ -58,9 +104,11 @@ class MainWindow(QtWidgets.QMainWindow):
                 
                 xaz = result.az.degree
                 xalt = (90-result.alt.degree)
-                cmd = "G0 X%.3f Y%.3f\r\n" % (xaz, xalt)
-                print(cmd)
-                #self.send_line(cmd)
+                if self.state == STATE_READY:
+                    cmd = "G0 X%.3f Y%.3f\r\n" % (xaz, xalt)
+                    print(cmd)
+                    self.state = STATE_SENDING_COMMAND
+                    self.send_line(cmd)
 
     def down_button_clicked(self):
         self.send_line("G0 Y-10")
